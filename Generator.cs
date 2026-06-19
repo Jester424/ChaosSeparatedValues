e6cb82577fc31b8c213d406e4c1f4889125da7a3
Nicholas Nicosia <nnicosi@wgu.edu>
2026-06-19 16:20:14 -0700

    Moved data generator to its own class

---
+++ Generator.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaosSeparatedValues
+{
+    public static class Generator
+    {
+        private static readonly Faker<MailingRecord> faker = new Faker<MailingRecord>()
+            .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+            .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+            .RuleFor(x => x.LastName, f => f.Name.LastName())
+            .RuleFor(x => x.Address, f => f.Address.StreetAddress())
+            .RuleFor(x => x.City, f => f.Address.City())
+            .RuleFor(x => x.State, f => f.Address.StateAbbr())
+            .RuleFor(x => x.Zip, f => f.Address.ZipCode());
+
+        public static MailingRecord Generate()
+        {
+            return faker.Generate();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,16 +20,6 @@ namespace ChaosSeparatedValues
 
             Logger.Info($"Output file: {filePath}");
 
-
-            var faker = new Faker<MailingRecord>()
-                .RuleFor(x => x.Id, f => f.IndexFaker + 1)
-                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
-                .RuleFor(x => x.LastName, f => f.Name.LastName())
-                .RuleFor(x => x.Address, f => f.Address.StreetAddress())
-                .RuleFor(x => x.City, f => f.Address.City())
-                .RuleFor(x => x.State, f => f.Address.StateAbbr())
-                .RuleFor(x => x.Zip, f => f.Address.ZipCode());
-
             using var writer = new StreamWriter(filePath);
             using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
@@ -42,7 +32,7 @@ namespace ChaosSeparatedValues
 
             for (int i = 0; i < recordCount; i++)
             {
-                var record = faker.Generate();
+                var record = Generator.Generate();
                 csv.WriteRecord(record);
                 csv.NextRecord();
 
