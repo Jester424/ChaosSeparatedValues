43a94aa665e4a792c46c5dd66ae1a4b48afff57b
Nicholas Nicosia <nnicosi@wgu.edu>
2026-06-19 16:03:58 -0700

    Moved MailingRecord model to its own file

---
+++ MailingRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaosSeparatedValues
+{
+    public class MailingRecord
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,17 +6,6 @@ using System.Globalization;
 
 namespace ChaosSeparatedValues
 {
-    public class MailingRecord
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public string State { get; set; } = string.Empty;
-        public string Zip { get; set; } = string.Empty;
-    }
-
     public class Program
     {
         public static void Main(string[] args)
