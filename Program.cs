88d2d707efb1d8b7b6181cfa41c3621379db42a1
Nicholas Nicosia <nnicosi@wgu.edu>
2026-06-19 14:21:42 -0700

    Initial commit: CSV generator using Bogus and CsvHelper

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Bogus;
+using CsvHelper;
+using System.Globalization;
+
+public class MailingRecord
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string Zip { get; set; } = string.Empty;
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Phase 1: Generate synthetic mailing data and export to CSV
+
+        string outputDirectory = "Data";
+        Directory.CreateDirectory(outputDirectory);
+        string filePath = Path.Combine(outputDirectory, "mailing.csv");
+
+
+        var faker = new Faker<MailingRecord>()
+            .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+            .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+            .RuleFor(x => x.LastName, f => f.Name.LastName())
+            .RuleFor(x => x.Address, f => f.Address.StreetAddress())
+            .RuleFor(x => x.City, f => f.Address.City())
+            .RuleFor(x => x.State, f => f.Address.StateAbbr())
+            .RuleFor(x => x.Zip, f => f.Address.ZipCode());
+
+        int recordCount = 10000;
+        List<MailingRecord> records = faker.Generate(recordCount);
+        using var writer = new StreamWriter(filePath);
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteRecords(records);
+
+        Console.WriteLine($"Created {records.Count:N0} records");
+    }
+}
