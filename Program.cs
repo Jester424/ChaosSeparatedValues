d7846963bd16ca1b4822a91401860ec2c9034bd2
Nicholas Nicosia <nnicosi@wgu.edu>
2026-06-20 14:55:20 -0700

    Moved clean and degraded data creation in to methods

--- Program.cs
+++ Program.cs
@@ -23,16 +23,23 @@ namespace ChaosSeparatedValues
             string cleanFilePath = Path.Combine(outputDirectory, "clean_list.csv");
             logger.Info($"Clean output file: {cleanFilePath}");
 
-
-
-
-
-
-
-
             int recordCount = 10000;
-            logger.Info($"Requested record count: {recordCount:N0}");
 
+            GenerateCleanDataFile(logger, cleanFilePath, recordCount);
+
+            logger.Info($"Finished generating {recordCount:N0} records");
+            logger.Info($"Beginning data degradation");
+            Thread.Sleep(2000);
+
+            string degradedFilePath = Path.Combine(outputDirectory, "degraded_list.csv");
+            logger.Info($"Degraded output file: {degradedFilePath}");
+
+            GenerateDegradedDataFile(logger, cleanFilePath, degradedFilePath);
+        }
+
+        private static void GenerateCleanDataFile(Logger logger, string cleanFilePath, int recordCount)
+        {
+            logger.Info($"Requested record count: {recordCount:N0}");
             using var writer = new StreamWriter(cleanFilePath);
             using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
             CsvExporter.WriteHeader(csv);
@@ -47,43 +54,18 @@ namespace ChaosSeparatedValues
                     logger.Info($"Generated {i:N0} records");
                 }
             }
+        }
 
-
-
-            // CsvExporter.Export(cleanFilePath, recordCount, logger);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            logger.Info($"Finished generating {recordCount:N0} records");
-            logger.Info($"Beginning data degradation");
-            Thread.Sleep(2000);
-
-            string degradedFilePath = Path.Combine(outputDirectory, "degraded_list.csv");
-            logger.Info($"Degraded output file: {degradedFilePath}");
-
+        private static void GenerateDegradedDataFile(Logger logger, string cleanFilePath, string degradedFilePath)
+        {
             using var reader = new StreamReader(cleanFilePath);
             using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-            using var xwriter = new StreamWriter(degradedFilePath);
-            using var csvWriter = new CsvWriter(xwriter, CultureInfo.InvariantCulture);
+            using var writer = new StreamWriter(degradedFilePath);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
-            csvWriter.WriteHeader<MailingRecord>();
-            csvWriter.NextRecord();
+            csv.WriteHeader<MailingRecord>();
+            csv.NextRecord();
 
             double degradationRate = 0.10;
 
@@ -96,10 +78,9 @@ namespace ChaosSeparatedValues
                     degradedRecordCount++;
                     DataDegrader.Degrade(record);
                 }
-                csvWriter.WriteRecord(record);
-                csvWriter.NextRecord();
+                csv.WriteRecord(record);
+                csv.NextRecord();
             }
-
             logger.Info($"Degradation complete for {degradedRecordCount} records");
         }
     }
