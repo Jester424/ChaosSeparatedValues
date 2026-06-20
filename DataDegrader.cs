802185fc743b4ab2e884e0c5210c1e983ecc6b61
Nicholas Nicosia <nnicosi@wgu.edu>
2026-06-20 12:53:41 -0700

    Added data degradation

---
+++ DataDegrader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaosSeparatedValues
+{
+    public static class DataDegrader
+    {
+        public static void Degrade(MailingRecord record)
+        {
+            record.City = null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,15 +16,46 @@ namespace ChaosSeparatedValues
             Directory.CreateDirectory(outputDirectory);
 
             string cleanFilePath = Path.Combine(outputDirectory, "clean_list.csv");
-            Logger.Info($"Output file: {cleanFilePath}");
+            Logger.Info($"Clean output file: {cleanFilePath}");
 
 
-            int recordCount = 500;
+            int recordCount = 10000;
             Logger.Info($"Requested record count: {recordCount:N0}");
 
             CsvExporter.Export(cleanFilePath, recordCount);
 
             Logger.Info($"Finished generating {recordCount:N0} records");
+            Logger.Info($"Beginning data degradation");
+            Thread.Sleep(2000);
+
+            string degradedFilePath = Path.Combine(outputDirectory, "degraded_list.csv");
+            Logger.Info($"Degraded output file: {degradedFilePath}");
+
+            using var reader = new StreamReader(cleanFilePath);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            using var writer = new StreamWriter(degradedFilePath);
+            using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            csvWriter.WriteHeader<MailingRecord>();
+            csvWriter.NextRecord();
+
+            double degradationRate = 0.10;
+
+            var random = new Random();
+            int degradedRecordCount = 0;
+            foreach (var record in csvReader.GetRecords<MailingRecord>())
+            {
+                if (random.NextDouble() < degradationRate)
+                {
+                    degradedRecordCount++;
+                    DataDegrader.Degrade(record);
+                }
+                csvWriter.WriteRecord(record);
+                csvWriter.NextRecord();
+            }
+
+            Logger.Info($"Degradation complete for {degradedRecordCount} records");
         }
     }
 }
